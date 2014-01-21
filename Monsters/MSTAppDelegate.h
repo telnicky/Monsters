99b84b972cb6bd6eaff60943279648b58499643f
Travis Elnicky <travis.elnicky@gmail.com>
2014-01-20 20:17:41 -0700

    Initial Commit

---
+++ Monsters/MSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MSTAppDelegate.h
+//  Monsters
+//
+//  Created by travis elnicky on 1/20/14.
+//  Copyright (c) 2014 Travis Elnicky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
